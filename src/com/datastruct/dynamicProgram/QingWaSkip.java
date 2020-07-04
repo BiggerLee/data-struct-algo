ddf7712150edbc9684bd3179a2e99db4a695dc80
BiggerLee <18822265532@163.com>
2020-07-04 09:30:11 +0800

    青蛙跳

--- src/com/datastruct/dynamicProgram/QingWaSkip.java
+++ src/com/datastruct/dynamicProgram/QingWaSkip.java
@@ -1,6 +1,9 @@
-package com.datastruct;
+package com.datastruct.dynamicProgram;
 
-public class DynamicProgram {
+/**
+ *  问题描述：一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法
+ */
+public class QingWaSkip {
     public static void main(String[] args) {
         System.out.println(qingWaSkip(5));
     }
